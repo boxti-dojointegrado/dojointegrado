a4ddfa3080769e266d28aaa39e126cdc45107300
Pedro Pinheiro <pedro.pinheiro@boxti.com.br>
2021-12-03 17:32:06 -0300

    feat: add address repository

---
+++ src/BoxTI.DojoIntegrado.Services/AutoMapper/MappingProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using BoxTI.DojoIntegrado.Domain.Entities;
+using BoxTI.DojoIntegrado.Services.DTOs;
+
+namespace BoxTI.DojoIntegrado.Services.AutoMapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Address, AddressDTO>().ReverseMap();
+        }
+    }
+}
